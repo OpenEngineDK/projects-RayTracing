01ff72f54cd5c099be4ddd61c9fc96a3cdff3f6e
asgerhoedt <asgerhoedt@gmail.com>
2010-10-08 09:44:05 +0000

    New dragon scene

--- main.cpp
+++ main.cpp
@@ -14,21 +14,35 @@
 #include <Logging/Logger.h>
 #include <Logging/StreamLogger.h>
 #include <Meta/Config.h>
+#include <Renderers/DataBlockBinder.h>
+#include <Resources/ResourceManager.h>
+#include <Scene/PointLightNode.h>
 #include <Scene/SceneNode.h>
+#include <Scene/RenderStateNode.h>
 
 // SDL extension
 #include <Display/SDLEnvironment.h>
 
+// Assimp
+#include <Resources/AssimpResource.h>
+
 // OpenGL
 #include <Renderers/OpenGL/Renderer.h>
+#include <Renderers/OpenGL/LightRenderer.h>
 #include <Display/OpenGL/RenderCanvas.h>
 
 // Generic Handler
 #include <Utils/BetterMoveHandler.h>
 #include <Utils/QuitHandler.h>
 
+// Mesh Utils
+#include <Utils/MeshCreator.h>
+#include <Utils/MeshTransformer.h>
+
 // Photon Mapping stuff
 #include <Renderers/OpenGL/PhotonRenderingView.h>
+
+#include <Utils/CUDA/Utils.h>
 
 // name spaces that we will be using.
 // this combined with the above imports is almost the same as
@@ -40,6 +54,110 @@ using namespace OpenEngine::Logging;
 using namespace OpenEngine::Renderers::OpenGL;
 using namespace OpenEngine::Scene;
 using namespace OpenEngine::Utils;
+
+ISceneNode* SetupScene(){
+
+    RenderStateNode* rsNode = new RenderStateNode();
+    rsNode->DisableOption(RenderStateNode::BACKFACE);
+    rsNode->EnableOption(RenderStateNode::LIGHTING);
+    rsNode->EnableOption(RenderStateNode::COLOR_MATERIAL);
+
+    TransformationNode* lightTrans = new TransformationNode();
+    lightTrans->SetPosition(Vector<3, float>(0.0f, 4.0f, 0.0f));
+    rsNode->AddNode(lightTrans);
+
+    PointLightNode* light = new PointLightNode();
+    Vector<4, float> lightColor = Vector<4, float>(255.0f, 235.0f, 205.0f, 255.0f) / 255.0f;
+    light->ambient = lightColor * 0.3;
+    light->diffuse = lightColor * 0.7;
+    light->specular = lightColor * 0.3;
+    lightTrans->AddNode(light);
+
+    ISceneNode* cornellBox = new SceneNode();
+    rsNode->AddNode(cornellBox);
+
+    MeshPtr plane = MeshCreator::CreatePlane(10, 1);
+    IDataBlockPtr c = IDataBlockPtr(new DataBlock<4, float>(plane->GetGeometrySet()->GetSize(),
+                                                                Vector<4, float>(1.0f)));
+    plane->GetGeometrySet()->AddAttributeList("color", c);
+    
+    MeshPtr top = MeshTransformer::Rotate(plane, Quaternion<float>(PI, 0.0f, 0.0f));
+    top = MeshTransformer::Translate(top, Vector<3,float>(0.0f, 5.0f, 0.0f));
+    MeshNode* topNode = new MeshNode(top);
+    cornellBox->AddNode(topNode);
+
+    MeshPtr bottom = MeshTransformer::Translate(plane, Vector<3,float>(0.0f, -5.0f, 0.0f));
+    MeshNode* bottomNode = new MeshNode(bottom);
+    cornellBox->AddNode(bottomNode);
+    
+    MeshPtr back = MeshTransformer::Rotate(plane, Quaternion<float>(PI/2.0f, 0.0f, 0.0f));
+    back = MeshTransformer::Translate(back, Vector<3,float>(0.0f, .0f, -5.0f));
+    MeshNode* backNode = new MeshNode(back);
+    cornellBox->AddNode(backNode);
+
+    MeshPtr front = MeshTransformer::Rotate(plane, Quaternion<float>(PI/-2.0f, 0.0f, 0.0f));
+    front = MeshTransformer::Translate(front, Vector<3,float>(0.0f, .0f, 5.0f));
+    MeshNode* frontNode = new MeshNode(front);
+    cornellBox->AddNode(frontNode);
+
+    plane = MeshCreator::CreatePlane(10, 1, Vector<3, float>(1.0f, 0.0f, 0.0f));
+    c = IDataBlockPtr(new DataBlock<4, float>(plane->GetGeometrySet()->GetSize(),
+                                              Vector<4, float>(1.0f, 0.0f, 0.0f, 1.0f)));
+    plane->GetGeometrySet()->AddAttributeList("color", c);
+    MeshPtr left = MeshTransformer::Rotate(plane, Quaternion<float>(0.0f, 0.0f, PI/-2.0f));
+    left = MeshTransformer::Translate(left, Vector<3,float>(-5.0f, 0.0f, 0.0f));
+    MeshNode* leftNode = new MeshNode(left);
+    cornellBox->AddNode(leftNode);
+
+    plane = MeshCreator::CreatePlane(10, 1, Vector<3, float>(0.0f, 0.0f, 0.8f));
+    c = IDataBlockPtr(new DataBlock<4, float>(plane->GetGeometrySet()->GetSize(),
+                                              Vector<4, float>(0.0f, 0.0f, 0.8f, 1.0f)));
+    plane->GetGeometrySet()->AddAttributeList("color", c);
+    MeshPtr right = MeshTransformer::Rotate(plane, Quaternion<float>(0.0f, 0.0f, PI/2.0f));
+    right = MeshTransformer::Translate(right, Vector<3,float>(5.0f, 0.0f, 0.0f));
+    MeshNode* rightNode = new MeshNode(right);
+    cornellBox->AddNode(rightNode);
+
+    // Dragon
+
+    TransformationNode* dragonTrans = new TransformationNode();
+    dragonTrans->SetScale(Vector<3, float>(40, 40, 40));
+    dragonTrans->SetPosition(Vector<3, float>(0, -7, 0));
+    cornellBox->AddNode(dragonTrans);
+
+    IModelResourcePtr duckRes = ResourceManager<IModelResource>::Create("projects/PhotonMapping/data/dragon/dragon_vrip_res4.ply");
+    duckRes->Load();
+    MeshNode* dragon = (MeshNode*) duckRes->GetSceneNode()->GetNode(0)->GetNode(0);
+
+    GeometrySetPtr dragonGeom = dragon->GetMesh()->GetGeometrySet();
+    IDataBlockPtr color = dragonGeom->GetDataBlock("color");
+    if (color != NULL){
+        *color *= 0.0f;
+        *color += Vector<3, float>(0.0f, 165.0f/255.0f, 101.0f/255.0f);
+    }else{
+        IDataBlockPtr vertices = dragonGeom->GetDataBlock("vertex");
+        float *c = new float[vertices->GetSize() * 4];
+        for (unsigned int i = 0; i < vertices->GetSize() * 4; ){
+            c[i++] = 0.0f;
+            c[i++] = 165.0f/255.0f;
+            c[i++] = 101.0f/255.0f;
+            c[i++] = 1.0f;
+        }
+        color = Float4DataBlockPtr(new DataBlock<4, float>(vertices->GetSize(), c));
+        dragonGeom = GeometrySetPtr(new GeometrySet(vertices, 
+                                                    dragonGeom->GetDataBlock("normal"),
+                                                    IDataBlockList(), color));
+        Mesh* dragonMesh = new Mesh(dragon->GetMesh()->GetIndices(),
+                                    dragon->GetMesh()->GetType(),
+                                    dragonGeom, dragon->GetMesh()->GetMaterial());
+        dragon = new MeshNode(MeshPtr(dragonMesh));
+    }
+
+    duckRes->Unload();
+    dragonTrans->AddNode(dragon);
+    
+    return rsNode;
+}
 
 /**
  * Main method for the first quarter project of CGD.
@@ -61,17 +179,26 @@ int main(int argc, char** argv) {
     engine->ProcessEvent().Attach(*env);
     engine->DeinitializeEvent().Attach(*env);
     
+    IRenderer* renderer = new Renderer();
+
+    ResourceManager<IModelResource>::AddPlugin(new AssimpPlugin());
+
+    ISceneNode* scene = SetupScene();
+
+    DataBlockBinder* bob = new DataBlockBinder(*renderer);
+    bob->Bind(*scene);
+
     IFrame& frame = env->CreateFrame();
 
     Camera* camera  = new Camera(*(new PerspectiveViewingVolume(1, 4000)));
 
     PhotonRenderingView* renderingview = new PhotonRenderingView();
-    IRenderer* renderer = new Renderer();
     renderer->InitializeEvent().Attach(*renderingview);    
     renderer->PreProcessEvent().Attach(*renderingview);
     renderer->ProcessEvent().Attach(*renderingview);
 
-    ISceneNode* scene = new SceneNode();
+    LightRenderer *lightRenderer = new LightRenderer();
+    renderer->PreProcessEvent().Attach(*lightRenderer);
 
     RenderCanvas* canvas = new RenderCanvas();
     canvas->SetViewingVolume(camera);
@@ -98,5 +225,3 @@ int main(int argc, char** argv) {
     // Return when the engine stops.
     return EXIT_SUCCESS;
 }
-
-
